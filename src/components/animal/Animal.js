426ddaa803b6954b8010cd704f00169c81304a0d
Steve Brownlee <chortlehoort@gmail.com>
2020-01-02 12:15:03 -0600

    Chapter 1 example code

---
+++ src/components/animal/Animal.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Animals.css"
+
+export default () => (
+    <section className="animal">
+        <h3 className="animal__name">Doodles</h3>
+        <div className="animal__breed">Breed: Poodle</div>
+    </section>
+)
---
+++ src/components/location/Location.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Locations.css"
+
+export default () => (
+    <section className="location">
+        <h3 className="location__name">Nashville Kennels Noth</h3>
+        <div className="location__address">200 Main Street</div>
+    </section>
+)
