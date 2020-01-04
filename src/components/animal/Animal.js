15d310c4ad9b92044616f98dbf391bbae9bbf15c
Steve Brownlee <chortlehoort@gmail.com>
2020-01-03 19:10:16 -0600

    Multiple providers

--- src/components/animal/Animal.js
+++ src/components/animal/Animal.js
@@ -1,11 +1,11 @@
 import React from "react"
 import "./Animals.css"
 
-export default ({ animal }) => (
+export default ({ animal, customer, location }) => (
     <section className="animal">
         <h3 className="animal__name">{ animal.name }</h3>
         <div className="animal__breed">{ animal.breed }</div>
-        <div className="animal__location">Location: { animal.locationId }</div>
-        <div className="animal__owner">Customer: { animal.customerId }</div>
+        <div className="animal__location">Location: { location.name }</div>
+        <div className="animal__owner">Customer: { customer.name }</div>
     </section>
 )
